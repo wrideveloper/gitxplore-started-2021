2c812011756d97aee7c2d6a0dde75b3fdb0a692d
DhayuIntan <91419607+DhayuIntan@users.noreply.github.com>
2021-10-30 20:56:14 +0700

    PresensiIntan

---
+++ mdFile/DhayuIntan/TugasSatu.java
@@ -0,0 +1,17 @@
+public class TugasSatu{
+
+	public static void main(String[] args) {
+String kampus = "Polinema";
+char kelas = 'Z';
+int tingkat = 1;
+int bilanganBulat = 10;
+float bilanganPecahan = 3.33333F;
+char karakter = 'C';
+System.out.println (String.format("Saya mahasiswa %s kelas %s%s", kampus, tingkat, kelas));
+System.out.println ("Saya sedang belajar menampilkan nilai");
+System.out.println ("Bilangan bulat\s\s" + bilanganBulat);
+System.out.println ("Bilangan pecahan\s\s" + bilanganPecahan);
+System.out.println ("Karakter\s\s" + karakter);
+
+}
+}
