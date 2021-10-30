a3161ea28d64a00e7a05fff41aa09b643da1f963
Kansha Maulidya Shyfa <kanshalidya12@gmail.com>
2021-10-30 21:55:41 +0700

    presensi kansha

---
+++ presensi/Kansha Maulidya/presensi.java
@@ -0,0 +1,11 @@
+class presensi {
+	public static void main(String[] args) {
+	System.out.println("-------------------------------"); 
+ 	System.out.println("Biodata Mahasiswa Jurusan Teknologi Informasi");
+ 	System.out.println("-------------------------------"); 
+ 	System.out.println("Nama		: Kansha Maulidya Shyfa");
+ 	System.out.println("NIM 		: 2131710148");
+ 	System.out.println("Alamat 		: Prambon, Nganjuk");
+ 	System.out.println("NoHP 		: 087883233003");
+	}
+}
