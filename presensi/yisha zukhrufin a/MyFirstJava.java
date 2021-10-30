37d2e53a3ac7b784179f47b2532b9f5c999fca4c
YishaZukhrufin <yishaangeli342@gmail.com>
2021-10-30 21:39:15 +0700

    presensi

---
+++ presensi/yisha zukhrufin a/MyFirstJava.java
@@ -0,0 +1,13 @@
+public class MyFirstJava{
+
+    public static void main(String[] args) {
+            System.out.println("-----------------------------------");
+	    System.out.println("Biodata Mahasiswa Jurusan Teknologi Informasi");
+            System.out.println("-----------------------------------");
+            System.out.println("Nama		:yisha zukhrufin angelyna");
+            System.out.println("Nim		:2141720013");
+            System.out.println("Alamat		:Desa mlandangan Kecamatan pace Kabupaten nganjuk");
+            System.out.println("Nohp		:085606224256");
+            System.out.println("Hobi		:voli");
+    }
+}
