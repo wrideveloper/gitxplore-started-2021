4618a029a34cf5f0e65a444842bdad35c4224c0c
Versa Feodora <veodorani@gmail.com>
2021-10-30 21:47:28 +0700

    absensi.java

---
+++ presensi/VersaFeodora/absensi.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class test{
+	public static void main(String[] args){
+
+		System.out.print("PRESENSI");
+		System.out.println("Versa Feodora / TI-1I");
+	}
+}
