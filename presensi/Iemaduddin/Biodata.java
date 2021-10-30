44c9f148f91d54845ca6c64236ecf7012cd9f2f8
Didin Arobidsh <iemaduddin17@gmail.com>
2021-10-30 20:46:36 +0700

    Menambah presensi Iemaduddin

---
+++ presensi/Iemaduddin/Biodata.java
@@ -0,0 +1,13 @@
+public class Biodata {
+    public static void main (String[] args){
+        System.out.println("-------------------------------------");
+        System.out.println("Mahasiswa Jurusan Teknologi Informasi");
+        System.out.println("-------------------------------------");
+        System.out.println("NAMA        : IEMADUDDIN");
+        System.out.println("NIM         : 2141720055");
+        System.out.println("TTL         : BANGKALAN, 13 AGUSTUS 2002");
+        System.out.println("ALAMAT      : KMP. LAOK SONGAI, BLEGA BANGKALAN");
+        System.out.println("NO. HP      : 082331311947");
+        System.out.println("EMAIL       : iemaduddin17@gmail.com");
+}
+}
